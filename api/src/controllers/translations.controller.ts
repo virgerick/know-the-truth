afb6df4c4df8f8eda1c0d6cb2f11c3d434a7cafe
Heriberto Alcantara <elfeo.0103@gmail.com>
2020-12-05 21:20:39 -0400

    get  version and languages

--- api/src/controllers/translations.controller.ts
+++ api/src/controllers/translations.controller.ts
@@ -1,13 +1,50 @@
-const translations= require('../assets/translations.json')
-const getTrastlation=async ()=>{
-   const translationsList:string[]=[]
-   for(let translation in translations)
-   {
-       translationsList.push(translation)
-   }
-    return   translationsList;
+const translations = require('../assets/translations.json')
+const getTranslationsName = async () => {
+    const translationsList: string[] = []
+    for (let translation in translations) {
+        translationsList.push(translation)
+    }
+    return translationsList;
 }
 
-export default{
-    getTrastlation
+const getByName = (name: string) => {
+
+    return translations[name];
+}
+
+const getAllLanguages = async () => {
+    const languages: any[] = []
+    try {
+        const translationsList = await getTranslationsName();
+        console.log(translationsList);
+
+        translationsList.forEach((name) => {
+            languages.push(getByName(name));
+        })
+    } catch (error) {
+        console.error(error);
+
+    }
+    return languages;
+}
+
+const getVersionByLanguage=(language:string)=>{
+    const versions:string[]=[]
+    for(let p in translations){
+        const item=translations[p];
+        if(item["language"]===language){
+            console.log(item);
+            versions.push(item.translation)
+        }
+            
+    }
+    return versions
+}
+
+
+export default {
+    getTranslationsName,
+    getByName,
+    getAllLanguages,
+    getVersionByLanguage
 }
--- api/src/routes/translations.routes.ts
+++ api/src/routes/translations.routes.ts
@@ -1,15 +1,35 @@
-import {Router} from 'express'
-import translationsController from '../controllers/translations.controller';
+import { Router } from 'express'
 import TranstationsController from '../controllers/translations.controller'
 
-const router=Router();
-router.get('/translations',async (req,res)=>{
-    const translations=await translationsController.getTrastlation();
-    
-    for(let p in translations){
-       console.log(p); 
-    }
+const router = Router();
+router.get('/translations', async (req, res) => {
+    const translations = await TranstationsController.getTranslationsName();
     return res.json(translations)
+})
+router.get('/translations/:name', async (req, res) => {
+    const { name } = req.params
+    const translation = TranstationsController.getByName(name);
+
+    return res.json(translation)
+})
+
+router.get('/Languages', async (req, res) => {
+    const allVersion = await TranstationsController.getTranslationsName();
+    const Languages: any[] = []
+    allVersion.forEach(name => {
+        const language = TranstationsController.getByName(name)["language"]
+        Languages.push(language)
+    });
+    //get a distint in an array
+    const result= Languages.filter((value, index, categoryArray) => categoryArray.indexOf(value) === index);
+    return res.json(result)
+})
+router.get('/translation/:language', async (req, res) => {
+    const { language } = req.params
+    console.log(language);
+    
+    const versions= TranstationsController.getVersionByLanguage(language);
+    return res.json(versions)
 })
 
 export default router
